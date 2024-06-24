d07bb9b2cf8fbdd16b3e8e3f786eb02c323fff30
GLetiwa <graceletiwa@gmail.com>
2024-06-24 11:43:49 -0700

    Templating footer

--- src/components/layouts/LandingPagesWrapper.jsx
+++ src/components/layouts/LandingPagesWrapper.jsx
@@ -3,7 +3,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import Footer from "./Footer";
+import Footer from "../partials/Footer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
